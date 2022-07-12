aead4a83f5f7024d2dab1cb78e8a2875dadb8d00
vishal <vishal.parmar@kevit.io>
2022-07-12 15:47:10 +0530

    remove un-used Code

--- index.js
+++ index.js
@@ -9,14 +9,6 @@ app.get('/', function (req, res) {
     res.send(`Hello GET. Users are: ${userCount}`);
  })
 
-
-// This responds a POST request for the homepage
-app.post('/', function (req, res) {
-    console.log("Got a POST request for the homepage");
-    res.send('Hello POST');
-})
-
-
 // This responds a DELETE request for the /del_user page.
 app.delete('/del_user', function (req, res) {
     console.log("Got a DELETE request for /del_user");
