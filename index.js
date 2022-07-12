169c568b5b36c923ef8cb95c3521bd930a647774
vishal <vishal.parmar@kevit.io>
2022-07-12 15:42:39 +0530

    Merge a user_api To Get User Count to 10

--- index.js
+++ index.js
@@ -10,13 +10,6 @@ app.get('/', function (req, res) {
  })
 
 
-// This responds a POST request for the homepage
-app.post('/', function (req, res) {
-    console.log("Got a POST request for the homepage");
-    res.send('Hello POST');
-})
-
-
 // This responds a DELETE request for the /del_user page.
 app.delete('/del_user', function (req, res) {
     console.log("Got a DELETE request for /del_user");
